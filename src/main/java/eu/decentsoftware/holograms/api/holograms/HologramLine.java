d1d76f74372e428c77cdb9926d393f8f06e54f1a
SissioMisio <manderioli25@gmail.com>
2025-06-01 10:16:54 +0200

    Add Half-Support for TextComponent tags parsing (e.g. <BLUE>, basically only for CrazyCrates)

--- src/main/java/eu/decentsoftware/holograms/api/holograms/HologramLine.java
+++ src/main/java/eu/decentsoftware/holograms/api/holograms/HologramLine.java
@@ -366,6 +366,32 @@ public class HologramLine extends HologramObject {
     // Parses custom replacements that can be defined in the config
     private String parseCustomReplacements() {
         if (!content.isEmpty()) {
+
+            /* Half-Parse the new TextComponent stuff (basically only for CrazyCrates :skull:) */
+            content = content
+                    .replace("<GREEN>", "&a")
+                    .replace("<AQUA>", "&b")
+                    .replace("<RED>", "&c")
+                    .replace("<LIGHT_PURPLE>", "&d")
+                    .replace("<YELLOW>", "&e")
+                    .replace("<WHITE>", "&f")
+                    .replace("<BLACK>", "&0")
+                    .replace("<DARK_BLUE>", "&1")
+                    .replace("<DARK_GREEN>", "&2")
+                    .replace("<DARK_AQUA>", "&3")
+                    .replace("<DARK_RED>", "&4")
+                    .replace("<DARK_PURPLE>", "&5")
+                    .replace("<GOLD>", "&6")
+                    .replace("<GRAY>", "&7")
+                    .replace("<DARK_GRAY>", "&8")
+                    .replace("<DARK_BLUE>", "&9")
+                    .replace("<OBFUSCATED>", "&k")
+                    .replace("<BOLD>", "&l")
+                    .replace("<STRIKETHROUGH>", "&m")
+                    .replace("<UNDERLINED>", "&n")
+                    .replace("<ITALIC>", "&o")
+                    .replace("<RESET>", "&r");
+
             for (Map.Entry<String, String> replacement : Settings.CUSTOM_REPLACEMENTS.entrySet()) {
                 content = content.replace(replacement.getKey(), replacement.getValue());
             }
